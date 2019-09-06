5ab0587ed39bf7e98b7657e1baab3fbdbeb0e74d
XX <fallwall19@gmail.com>
2019-09-06 15:18:16 -0400

    add action and pass in to from context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+  // const blogPosts = [
+  //   { title: '#1 It\'s a good day.' },
+  //   { title: '#2 Somewhere between summer and fall.' },
+  //   { title: '#3 You can\'t repair that.' },
+  //   { title: '#4 Demon cat wakes you up at 4 am.' },
+  // ];
+
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([...blogPosts, { title: `#${blogPosts.length + 1} Post` }]);
+  };
+
   return (
-    <BlogContext.Provider value={5}>
-    {children}
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
+      {children}
     </BlogContext.Provider>
   )
 };
